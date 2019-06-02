d0cc69d9ef76f7be8c75f2d16821807388dc953f
David Henderson <david.henderson82@gmail.com>
2019-06-02 13:42:11 -0700

    Initial commit

---
+++ wp-content/themes/simplent/footer.php
@@ -0,0 +1,15 @@
+        </div><!-- .site-content -->
+
+        <footer id="colophon" class="site-footer" role="contentinfo">
+            <div class="site-info container">
+            <?php /* translators: %s: WordPress */ ?>
+
+            </div>
+        </footer>
+
+    </div><!-- site-inner -->
+</div><!-- site -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
